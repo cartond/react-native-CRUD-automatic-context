5a0ffc4de1af8a0bbdc45a63c0b1dca3d6a65a15
Daniel Carton <cartond93@gmail.com>
2020-05-19 09:08:22 -0400

    Setup and testing provider

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,11 +3,17 @@ import React, { Children } from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  const blogPosts = [
+    { title: 'uno' },
+    { title: 'dos' },
+    { title: 'tres' }
+  ]
+
   return (
-    <BlogContext.Provider>
+    <BlogContext.Provider value={blogPosts}>
       {children}
     </BlogContext.Provider>
   )
 }
 
-// export default BlogContext;
+export default BlogContext;
