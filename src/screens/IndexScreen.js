09055181bb16a60674f121719aaa58a715e9e935
Daniel Carton <cartond93@gmail.com>
2020-05-21 00:09:15 -0400

    light styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,24 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons'; 
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context)
 
   return (
     <View>
-      <Text>Index Screen {state.length}</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList 
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather name="trash" style={styles.icon } />
+            </View>
+          )
         }}
       />
     </View>
@@ -21,6 +26,20 @@ const IndexScreen = () => {
 }
 
 const styles = StyleSheet.create({
+  row:{
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 10
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  },
   add:{
     marginTop: 20,
     borderColor: 'black',
